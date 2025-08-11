e72ba31fe147cd615fc38eccdbd7f9281ee0759e
rujeraporn <69622393+rujeraporn@users.noreply.github.com>
2025-08-11 16:54:33 +0700

    เพมไฟลโคดชดแรก

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+// https://vite.dev/config/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 80, // ถ้าไม่จำเป็นอย่าใช้ port 80 ตอน dev
+  },
   plugins: [react()],
 })
